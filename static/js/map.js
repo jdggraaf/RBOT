13f316cb6224c5c4bc0bca49018bfba13ce50d3a
neskk <neskk@oninet.pt>
2017-04-28 15:42:19 +0100

    Revert "Add IV/CP scanning support with a L30 account pool. (#1981)"
    This reverts commit bb3b02fbbcc6bb28c24a3f52c5addbab090bbe2c.

--- static/js/map.js
+++ static/js/map.js
@@ -423,7 +423,6 @@ function pokemonLabel(item) {
     var height = item['height']
     var gender = item['gender']
     var form = item['form']
-    var cp = item['cp']
 
     $.each(types, function (index, type) {
         typesDisplay += getTypeSpan(type)
@@ -436,17 +435,6 @@ function pokemonLabel(item) {
             <div>
                 IV: ${iv.toFixed(1)}% (${atk}/${def}/${sta})
             </div>
-            `
-
-        if (cp !== null) {
-            details += `
-            <div>
-                CP: ${cp}
-            </div>
-            `
-        }
-
-        details += `
             <div>
                 Moves: ${pMove1} / ${pMove2}
             </div>
